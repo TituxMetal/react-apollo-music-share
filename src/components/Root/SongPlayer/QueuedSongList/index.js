7b6bb1d1b882d40556b09bbd407e961a0aa98965
Titux Metal <github@lgdweb.fr>
2021-05-16 18:40:54 +0200

    Adding SongPlayer component in Root; basic component

---
+++ src/components/Root/SongPlayer/QueuedSongList/index.js
@@ -0,0 +1,1 @@
+export { default as QueuedSongList } from './QueuedSongList'
---
+++ src/components/Root/SongPlayer/SongPlayer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { QueuedSongList } from './QueuedSongList'
+
+const SongPlayer = () => (
+  <div>
+    <QueuedSongList />
+    SongPlayer
+  </div>
+)
+
+export default SongPlayer
