716cd77a8fac5d45b79c738a69aec40b2fba476b
Titux Metal <github@lgdweb.fr>
2021-05-17 12:48:29 +0200

    Updating AddSong component; shows an input field to add a song url and a button, when the button is clicked it opens the FormDialog component

--- src/components/Root/AddSong/AddSong.js
+++ src/components/Root/AddSong/AddSong.js
@@ -1,5 +1,22 @@
-import React from 'react'
+import { Grid } from '@material-ui/core'
+import React, { useState } from 'react'
 
-const AddSong = () => <div>AddSong</div>
+import { AddButton } from './AddButton'
+import { FormDialog } from './FormDialog'
+import { InputField } from './InputField'
+
+const AddSong = () => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false)
+
+  const handleCloseDialog = () => setDialogIsOpen(false)
+
+  return (
+    <Grid container alignItems='center'>
+      <FormDialog open={dialogIsOpen} onClose={handleCloseDialog} />
+      <InputField />
+      <AddButton onClick={() => setDialogIsOpen(true)}>Add</AddButton>
+    </Grid>
+  )
+}
 
 export default AddSong
