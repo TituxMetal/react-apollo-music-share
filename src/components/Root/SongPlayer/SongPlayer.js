fe9b2a7c0d2f8f9668ab4d3f3cc80e4e49e161d7
Titux Metal <github@lgdweb.fr>
2021-05-19 10:20:42 +0200

    Updating SongPlayer component in Root; displays the song player in a card

--- src/components/Root/SongPlayer/SongPlayer.js
+++ src/components/Root/SongPlayer/SongPlayer.js
@@ -1,12 +1,61 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  makeStyles
+} from '@material-ui/core'
 import React from 'react'
 
+import { Controls } from './Controls'
+import { Info } from './Info'
 import { QueuedSongList } from './QueuedSongList'
 
-const SongPlayer = () => (
-  <div>
-    <QueuedSongList />
-    SongPlayer
-  </div>
-)
+const songData = {
+  title: 'Hypnotize',
+  artist: 'System Of A Down',
+  thumbnail: 'https://i.ytimg.com/vi/KAEBfiaY6Rc/hqdefault.jpg'
+}
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    margin: theme.spacing(2)
+  },
+  mediaContainer: {
+    height: '140px',
+    margin: 'auto'
+  },
+  root: {
+    height: '100%',
+    width: '100%'
+  }
+}))
+
+const SongPlayer = () => {
+  const { container, mediaContainer, root } = useStyles()
+
+  return (
+    <>
+      <Card variant='outlined' className={container}>
+        <Grid container item xs={12} justify='space-between'>
+          <Grid item xs={9}>
+            <CardContent>
+              <Info artist={songData.artist} title={songData.title} />
+              <Controls />
+            </CardContent>
+          </Grid>
+          <Grid item xs={3} className={mediaContainer}>
+            <CardMedia
+              className={root}
+              component='img'
+              src={songData.thumbnail}
+            />
+          </Grid>
+        </Grid>
+      </Card>
+      <QueuedSongList />
+    </>
+  )
+}
 
 export default SongPlayer
