d6893e7b52c2c862eeb489c95cabf0fcd69c9505
Titux Metal <github@lgdweb.fr>
2021-05-19 07:15:41 +0200

    Updating SongList component in Root; generates a list of fake songs

--- src/components/Root/SongList/SongList.js
+++ src/components/Root/SongList/SongList.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-const SongList = () => <div>SongList</div>
+import { SongItem } from './SongItem'
+
+const songData = {
+  title: 'Hypnotize',
+  artist: 'System Of A Down',
+  thumbnail: 'https://i.ytimg.com/vi/KAEBfiaY6Rc/hqdefault.jpg'
+}
+
+const SongList = () =>
+  Array.from({ length: 10 }, () => songData).map((song, index) => (
+    <SongItem key={index} {...song} />
+  ))
 
 export default SongList
