79319004ead340fbfc6f8425240faae4bb000dd2
Titux Metal <github@lgdweb.fr>
2021-05-20 07:29:11 +0200

    Updating QueuedSongList component in SongPlayer; renders a list of queued songs whith DataList component

--- src/components/Root/SongPlayer/QueuedSongList/QueuedSongList.js
+++ src/components/Root/SongPlayer/QueuedSongList/QueuedSongList.js
@@ -1,5 +1,31 @@
+import { Grid, makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
 
-const QueuedSongList = () => <div>QueuedSongList</div>
+import { DataList } from '#root/components/DataList'
+
+import { QueuedSongItem } from './QueuedSongItem'
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    padding: theme.spacing(0, 2)
+  }
+}))
+
+const QueuedSongList = () => {
+  const { container } = useStyles()
+
+  return (
+    <Grid className={container} container>
+      <Grid item xs={12}>
+        <Typography gutterBottom variant='h4'>
+          Queue (3)
+        </Typography>
+      </Grid>
+      <Grid container>
+        <DataList itemComponent={QueuedSongItem} itemNumber={3} />
+      </Grid>
+    </Grid>
+  )
+}
 
 export default QueuedSongList
