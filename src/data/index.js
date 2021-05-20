51ac986c84bfe39968ec6d3182d82acf7c4b7e6e
Titux Metal <github@lgdweb.fr>
2021-05-20 07:15:59 +0200

    Adding song.js file in data; some fake data

---
+++ src/data/index.js
@@ -0,0 +1,1 @@
+export { default as songData } from './song'
---
+++ src/data/song.js
@@ -0,0 +1,7 @@
+const songData = {
+  title: 'Hypnotize',
+  artist: 'System Of A Down',
+  thumbnail: 'https://i.ytimg.com/vi/KAEBfiaY6Rc/hqdefault.jpg'
+}
+
+export default songData
