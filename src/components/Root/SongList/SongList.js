999770f6e4ee4b484ea5e352ea47cf921fab2cbc
Titux Metal <github@lgdweb.fr>
2021-05-20 07:40:13 +0200

    Updating SongList component in Root; uses the DataList component instead of hardcoded data

--- src/components/Root/SongList/SongList.js
+++ src/components/Root/SongList/SongList.js
@@ -1,16 +1,9 @@
 import React from 'react'
 
+import { DataList } from '#root/components/DataList'
+
 import { SongItem } from './SongItem'
 
-const songData = {
-  title: 'Hypnotize',
-  artist: 'System Of A Down',
-  thumbnail: 'https://i.ytimg.com/vi/KAEBfiaY6Rc/hqdefault.jpg'
-}
-
-const SongList = () =>
-  Array.from({ length: 10 }, () => songData).map((song, index) => (
-    <SongItem key={index} {...song} />
-  ))
+const SongList = () => <DataList itemComponent={SongItem} itemNumber={10} />
 
 export default SongList
